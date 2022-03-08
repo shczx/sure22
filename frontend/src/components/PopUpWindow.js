b5fbd7dbd69c18ca2da583bf2d08b285253e6350
Zhixuan Chen <shczx@umich.edu>
2022-03-07 20:54:51 -0500

    Final version

--- frontend/src/components/PopUpWindow.js
+++ frontend/src/components/PopUpWindow.js
@@ -5,8 +5,8 @@ function PopUpWindow(props) {
     const { input } = props;
     return (
         <form className="popup" onSubmit={props.handleInput}>
-            <label htmlFor="text">Enter a note for your selected text!</label>
-            <input id="text" type="text" value={input} onChange={props.handleInputChange} />
+            <label htmlFor="textBox">Enter a note for your selected text!</label>
+            <input id="textBox" type="text" value={input} onChange={props.handleInputChange} />
             <input id="submit" type="submit" value="Submit" />
         </form>
     )
